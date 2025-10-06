6b6cbc222e766f37b511edb2ee8f7072e1599eae
LizaA-hub <150455028+LizaA-hub@users.noreply.github.com>
2025-10-06 14:42:56 +0200

    timer low sfx and color

--- audio_manager.gd
+++ audio_manager.gd
@@ -5,10 +5,15 @@ extends Node
 @onready var pnj_stream : AudioStreamPlayer = %pnj_sfx
 @onready var ui_stream : AudioStreamPlayer = %ui_sfx
 @onready var walk_stream : AudioStreamPlayer = %player_walk
-#@export var walk_tracks : Array[AudioStream]
+@onready var timer_low : AudioStreamPlayer = %timer_low
 @export var game_over_sound : AudioStream
 @export var loot_tracks : Array[AudioStream]
 @export var punch_tracks : Array[AudioStream]
+
+enum TimerSoundState{OFF,FADE_IN,FADE_OUT,ON}
+var timer_sound_state : TimerSoundState = TimerSoundState.OFF
+var timer_sound_vol : float = 0
+var variation_speed : float = 1
 
 func _ready() -> void:
 	player_stream.play()
@@ -16,9 +21,36 @@ func _ready() -> void:
 	
 func change_music(track : String)->void:
 	music_stream["parameters/switch_to_clip"] = track
-#
-#func play_walk_sound()->void:
-	#_play_player_sfx(walk_tracks.pick_random())
+
+func _process(delta: float) -> void:
+	if !World.game_on : return
+	var game_timer : float = World.current_timer
+	if game_timer > 2 :
+		match timer_sound_state:
+			TimerSoundState.FADE_IN:
+				timer_sound_state = TimerSoundState.FADE_OUT
+			TimerSoundState.FADE_OUT:
+				timer_sound_vol -= variation_speed*delta
+				if timer_sound_vol <= 0:
+					timer_sound_vol = 0
+					timer_low.stop()
+					timer_sound_state = TimerSoundState.OFF
+			TimerSoundState.ON:
+				timer_sound_state = TimerSoundState.FADE_OUT
+	if game_timer <= 2 :
+		match timer_sound_state:
+			TimerSoundState.FADE_IN:
+				timer_sound_vol += variation_speed*delta
+				if timer_sound_vol >= 1:
+					timer_sound_vol = 1
+					timer_sound_state = TimerSoundState.ON
+			TimerSoundState.FADE_OUT:
+				timer_sound_state = TimerSoundState.FADE_IN
+			TimerSoundState.OFF:
+				timer_sound_state = TimerSoundState.FADE_IN
+				timer_low.play()
+				
+	timer_low.volume_linear = timer_sound_vol
 
 func play_game_over()->void:
 	_play_player_sfx(game_over_sound)
@@ -48,4 +80,6 @@ func toggle_walk_sound(value:bool)->void:
 		
 func _on_game_over()->void:
 	walk_stream.stop()
+	timer_low.stop()
+	timer_sound_vol = 0
 	
